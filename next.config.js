64539199714fd3c5ff71a8b58617f3d696e8904c
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-09-19 09:28:12 -0300

    feat(root): add domains for img optimization

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "img.alvearsupermercados.com.ar",
+      "www.alvearsupermercados.com.ar",
+      "www.alvearonline.com.ar",
+    ],
+  },
+};
