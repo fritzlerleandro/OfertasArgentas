fdc8af33a256bbaca28735d43a0f7e1962c8834e
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-09-19 09:30:35 -0300

    feat(root): tailwind and postcss default config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or false or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
